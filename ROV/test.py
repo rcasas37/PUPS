455424611be87b45fc31a968f458239a3e9bcbde
Landon Ledbetter <lledbetter22@gmail.com>
2018-08-31 00:32:16 +0000

    Created Hello world test program

--- ROV/test.py
+++ ROV/test.py
@@ -7,3 +7,11 @@
 
 #new stuff
 #New stuff added on Comp to view on Pi
+for i in range(10):
+	print("Hello, World! %d \n" %(i + 1))
+
+capstone_grade = 98
+
+print("Your capstone grade: %d" %capstone_grade )
+#print(capstone_grade)
+
