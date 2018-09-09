2f10b710bd32ac619fb7d2dfd9bee5d27481f785
lledbetter22 <lledbetter22@gmail.com>
2018-09-09 05:44:38 +0000

    Changed rov_README.md to README_rov_README.md to README_rov.md for programming ease of rov_skeleton.py

--- ROV/mysshtest.py
+++
@@ -1,5 +0,0 @@
-#this is a comment within nanon SSH terminal
-
-#The carrot '^' symbol represents control key ie the menu at the bottom of the nano window
-
-
--- ROV/test.py
+++ ROV/test.py
@@ -1,17 +1,15 @@
 #C:\Users\lledb\AppData\Local\Programs\Python\Python35-32\%compiles python%this is a test example .py file
-#this is a test example .py file
-#this is a test example .py file
+
+# this is a test example .py file
+# this file should run on startup based on the bash script I made:
+# /etc/init.d/boot_py_script  OR beause of the line I added in:
+# /etc/rc.local which runs after everython boots
 
 
 
-
-#new stuff
-#New stuff added on Comp to view on Pi
+# Test output
 for i in range(10):
 	print("Hello, World! %d \n" %(i + 1))
 
 capstone_grade = 98
-
 print("Your capstone grade: %d" %capstone_grade )
-#print(capstone_grade)
-
