f18b57b8a45a28e56e9161badc1c92ed3a98e3f7
Landon Ledbetter <lledbetter22@gmail.com>
2018-09-09 02:33:06 -0500

    Just playing around with fn definitions in python etc

--- ROV/rov_skeleton.py
+++ ROV/rov_skeleton.py
@@ -1,4 +1,47 @@
+"""
+Texas A&M University
+Electronic Systems Engineering Technology
+ESET-420 Capstone II
+Author: SEAL
+File: rov_skeleton.py
+--------
+Will contain the main loop and functions that run SEAL's rov. Including movement, 
+sensor readings, and communication to SEAL's cmd center.
+"""
+
+import sys	#Module provides access to fns maintained by the interpreter
+import os	#Module provides fns allowing one to use OS dependent functionality
 
 
-import sys
+#Initialize I2C bus slave addresses explicitly to default values
+###Set parameters that hold the slave addresses in Hex
 
+
+
+"""
+The main loop will control all of the operations of the ROV
+Parameters:
+
+Return:
+	0 on success, error code on failure
+Notes:
+	Initialized all sensor 
+"""
+def main():
+
+	"""write my comments here for a
+	multiple line comment"""
+	#Or here both work
+	"""This is the main python program for the ROV"""
+
+if __name__ == '__main__':
+	sys.exit(main())
+
+
+
+"""
+Obtain string commands from cmd center.
+"""
+def read_serial_port():
+
+	return;
--- ROV/test.py
+++ ROV/test.py
@@ -4,12 +4,41 @@
 # this file should run on startup based on the bash script I made:
 # /etc/init.d/boot_py_script  OR beause of the line I added in:
 # /etc/rc.local which runs after everython boots
+from decimal import Decimal #Module provides converting a string to decimal
+
+capstone_grade = 0
+
+#Program function definitions
+def main():
+	while True:
+		# Test output
+		#for i in range(10):
+		#	print("Hello, World! %d \n" %(i + 1))
+
+		#get usr input
+		capstone_grade1 = input("What grade do you want in capstone? ")
+		capstone_grade = Decimal(capstone_grade1) #convert str to double
+
+		print_grade(capstone_grade)
+
+		"""Main loop terminator must have "return" if used "break" we return
+		operation back to where it left off ie we only get out of loop and 
+		continue with main definition execution"""
+		if capstone_grade == 100:
+			print_finalgrade(capstone_grade)
+			return 0;
+			
+#fn definition
+def print_grade(capstone_grade):
+	print("Your capstone grade: %d" %capstone_grade)
+
+#fn definition
+def print_finalgrade(capstone_grade):
+	print("Goodbye. Your capstone grade: %d" %capstone_grade )
 
 
 
-# Test output
-for i in range(10):
-	print("Hello, World! %d \n" %(i + 1))
-
-capstone_grade = 98
-print("Your capstone grade: %d" %capstone_grade )
+##########################################################################
+#program execution of all functions etc
+main()
+#if we want more fns to execute after main then we put them here
