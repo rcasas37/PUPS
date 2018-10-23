486325716d4d294b6f453de0ee21d2a023f8b437
lledbetter22 <lledbetter22@gmail.com>
2018-10-22 22:00:21 -0500

    minor changes to test_uart.py

--- ROV/test_uart.py
+++ ROV/test_uart.py
@@ -6,7 +6,7 @@ import test_uart
 
 #def main():
 # Open serial port communication
-ser = serial.Serial(port='/dev/ttyS0', baudrate=57600, parity=serial.PARITY_NONE,
+ser = serial.Serial(port='/dev/ttyS0', baudrate=9600, parity=serial.PARITY_NONE,
                     stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)        # (physical port, baudrate, timeout interval)
 
 ser.flushInput()
